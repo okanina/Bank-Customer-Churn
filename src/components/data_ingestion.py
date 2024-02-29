1793ef458b74f6d107121a6a59479f65d5d74eb4
okanina <cynthiajiyane88@gmail.com>
2024-03-01 00:17:39 +0200

    logging and exception

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class dataIngestionConfig:
+    pass
+
+class dataIngestion:
+    def __init__(self)
