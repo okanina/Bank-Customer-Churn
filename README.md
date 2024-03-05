2a9b47b1ce68306d104ea171a0b7982831da8d31
okanina <cynthiajiyane88@gmail.com>
2024-03-05 07:12:05 +0200

    data ingestion

---
+++ README.md
@@ -0,0 +1,21 @@
+1.Problem statement
+
+The problem statement for this project is customers who churns banking industry.  customer churn occurs when the customer terminate his/her relationship with a business or organization.
+
+Customer churn affect any business or organization future growth. In this project I intend to use machine Learning techniques to find patterns and then predict customers who would leave Bank XYZ.
+
+2. Data Source
+
+I downloaded the dataset from Kaggle.com[https://www.kaggle.com/datasets/shantanudhakadd/bank-customer-churn-prediction].It is a csv file, I used pandas to read it into pandas dataframe, whih makes it simpler to analyze the data.It has 10000 observations with 12 features including the target variable.This is a supervised machine learning project which predict a yes or no making it a binary classification problem.
+
+The data has no missing or duplicate values. It consist of 3 datatypes, int64, floats, and objects(string values). It is highly imbalance, with 20.4 % positive class and 79.63 % negative class.
+
+The following are the features present in the dataset:
+
+Basic Statistical Summary:
+Unique Column Values:
+Data Preprocessing and Feature engineering:
+Data limitations:
+
+Data Splitting Strategy and Evaluation Metrics
+
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,10 +1,43 @@
 import pandas as pd
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 @dataclass
 class dataIngestionConfig:
-    pass
+    train_data=os.path.join('artifacts',"train.csv")
+    test_data=os.path.join("artifacts", "test.csv")
+    raw_data=os.path.join("artifacts", "data.csv")
 
 class dataIngestion:
-    def __init__(self)
+    def __init__(self):
+        self.ingestion_config=dataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered data ingestion method.")
+        try:
+            df=pd.read_csv("data\Bank_Customer_Churn_Prediction.csv")
+            logging.info("Read the dataset to datframe.")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data, index=False, header=True)
+
+            logging.info("Train, Test split initiated.")
+
+            train_set, test_set=train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data, index=False, header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data, index=False, header=True)
+
+            logging.info("Data ingestion, complete")
+
+            return (self.ingestion_config.train_data, self.ingestion_config.test_data)
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
